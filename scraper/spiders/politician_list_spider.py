20266c46e151cdae7968a019dc6de38c38258068
Lahiru Udayanga <lahirudesilva.17@cse.mrt.ac.lk>
2021-10-25 02:27:55 +0530

    politician list scraping implementation

---
+++ scraper/spiders/politician_list_spider.py
@@ -0,0 +1,28 @@
+import scrapy
+from string import ascii_lowercase
+import json
+
+class PoliticianSpider(scrapy.Spider):
+    """
+    PoliticianSpider scrapes the list of politicians from the parliament.lk website.
+    """
+    name = "politicians"
+    politicians = []
+
+    def start_requests(self):
+        """
+        Sends a HTTP request for each of the alphabetic letter.
+        """
+        url = "https://www.parliament.lk/members-of-parliament/index2.php?option=com_members&task=all&tmpl=component&letter={letter}"
+        for letter in ascii_lowercase:
+            yield scrapy.Request(url=url.format(letter=letter), callback=self.parse)
+
+    def parse(self, response):
+        """
+        Concatenates the JSON lists for each of the HTTP requests and write the output to a file as a JSON array.
+        """
+        self.politicians.extend(response.json())
+        self.log(len(self.politicians))
+        if len(self.politicians) == 225:
+            file = open("politicians.json", "w")
+            json.dump(self.politicians, file)
