805f85d292b059625e22ad7936ddb0a1da09da2c
Lahiru Udayanga <lahirudesilva.17@cse.mrt.ac.lk>
2021-11-23 15:13:18 +0530

    index settings added

--- elastic-search/create-index.py
+++ elastic-search/create-index.py
@@ -1,7 +1,6 @@
-import json
-
 from management import EsManagement
 from mappings import politician_mapping
+from settings import politician_settings
 
 es_connection = EsManagement()
-es_connection.create_index(index_name="sl_politicians", mapping=politician_mapping)
+es_connection.create_index(index_name="sl_politicians", mapping=politician_mapping, settings=politician_settings)
--- elastic-search/management.py
+++ elastic-search/management.py
@@ -4,21 +4,21 @@ import logging
 import json
 import glob
 
-logging.basicConfig(filename="es.log", level=logging.INFO)
+logging.basicConfig(filename="es.log", level=logging.DEBUG)
 
 class EsManagement:
     def __init__(self) -> None:
         self.es_client = Elasticsearch(HOST="http://localhost",PORT=9200)
         logging.info(self.es_client.ping())
 
-    def create_index(self, index_name: str, mapping: Dict) -> None:
+    def create_index(self, index_name: str, mapping: Dict, settings) -> None:
         """
         Create an ES index.
         :param index_name: Name of the index.
         :param mapping: Mapping of the index
         """
         logging.info(f"Creating index {index_name} with the following schema: {json.dumps(mapping, indent=2)}")
-        self.es_client.indices.create(index=index_name, ignore=400, body=mapping)
+        self.es_client.indices.create(index=index_name, ignore=400, mappings=mapping, settings=settings)
 
     def populate_index(self, path: str, index_name: str) -> None:
         files = glob.glob(path+"/*")
