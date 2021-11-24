ebe88d57eb5a57b72c57caca27701e9d59e4cfef
Lahiru Udayanga <lahirudesilva.17@cse.mrt.ac.lk>
2021-11-25 02:01:20 +0530

    removed stop words

--- flask/query_processor.py
+++ flask/query_processor.py
@@ -5,7 +5,7 @@ from sinling import word_splitter
 pmKeywords = ["මන්ත්‍රීවරයා", "මන්ත්‍රීවරයා", "මැතිතුමා", "මැති", "ගරු", "පා.ම."]
 locationKeywords = ["ප්‍රදේශයේ", "දිස්ත්‍රික්කයේ", "දිස්ත්‍රික්කය", "ප්‍රදේශය"]
 committeeKeywords = ["කමිටුව" "කමිටුවේ"]
-partyKeywords = ["පක්ෂයේ", "පක්ෂය", "සන්ධානය", "සන්ධානයේ"]
+partyKeywords = ["පක්ෂයේ", "පක්ෂය", "සන්ධානය", "සන්ධානයේ", "පෙරමුණ"]
 
 tokenizer = SinhalaTokenizer()
 
@@ -20,19 +20,29 @@ class QueryProcessor:
     def getBoostList(self, tokens):
         field_priorities = {}
         for token in tokens:
-            splitted_words = word_splitter.split(token)
-            print(splitted_words)
+            # splitted_words = word_splitter.split(token)
 
-            if (token in pmKeywords or splitted_words['affix'] in pmKeywords or splitted_words['base'] in pmKeywords):
+            # if (token in pmKeywords or splitted_words['affix'] in pmKeywords or splitted_words['base'] in pmKeywords):
+            #     field_priorities['name'] = 2.0
+            
+            # if (token in locationKeywords or splitted_words['affix'] in locationKeywords or splitted_words['base'] in locationKeywords):
+            #     field_priorities['electoral'] = 2.0
+
+            # if (token in committeeKeywords or splitted_words['affix'] in committeeKeywords or splitted_words['base'] in committeeKeywords):
+            #     field_priorities['committees'] = 2.0
+
+            # if (token in partyKeywords or splitted_words['affix'] in partyKeywords or splitted_words['base'] in partyKeywords):
+            #     field_priorities['party'] = 2.0
+            if (token in pmKeywords):
                 field_priorities['name'] = 2.0
             
-            if (token in locationKeywords or splitted_words['affix'] in locationKeywords or splitted_words['base'] in locationKeywords):
+            if (token in locationKeywords):
                 field_priorities['electoral'] = 2.0
 
-            if (token in committeeKeywords or splitted_words['affix'] in committeeKeywords or splitted_words['base'] in committeeKeywords):
+            if (token in committeeKeywords):
                 field_priorities['committees'] = 2.0
 
-            if (token in partyKeywords or splitted_words['affix'] in partyKeywords or splitted_words['base'] in partyKeywords):
+            if (token in partyKeywords):
                 field_priorities['party'] = 2.0
 
         boost_list = []
